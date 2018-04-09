dcca50c204b65caae76abf4f97adaac3f0e6c33b
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-09 20:44:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,12 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are #{katz_deli.length} in line."
 end
+
+def now_serving(katz_deli, name)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    person = katz_deli.shift
+    puts "Currently serving #{person}."
+  end
+end
